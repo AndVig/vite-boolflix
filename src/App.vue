12136da7197f22f35881d9522febe3c581967ace
AndVig <andrea.vigna2@gmail.com>
2024-06-07 19:08:02 +0200

    start milstone 1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script>
+export default{
+    name: Boolflix,
+    methods:{
+        search(){
 
+        },
+    }
+}
 </script>
 
 <template>
-  
+    <h1>Boolflix</h1>
+    <div>
+        <label for="">Ricerca</label>
+        <input type="text" id="search-movie">
+        <button @click="search">Cerca</button>
+    </div>
 </template>
 
 <style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-
+    <h1>Boolflix</h1>
+    <div>
+        <label for="">Ricerca</label>
+        <input type="text" id="search-movie">
+        <button></button>
+    </div>
 </template>
 
 <style>
