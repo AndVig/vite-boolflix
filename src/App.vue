f686940d82d5fd0bbc420ae902aa011e327fc8f2
AndVig <andrea.vigna2@gmail.com>
2024-06-08 09:21:39 +0200

    move Header section in AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,40 @@
 <script>
-import axios from 'axios';
 import {store} from './store'
+import AppHeader from './components/AppHeader.vue';
+
 
 export default{
     name: 'Boolflix',
+    components:{
+        AppHeader
+    },
     data(){
         return{
             store,
         }
     },
-    methods:{
-        searchMovie(){
-            const url =this.store.apiInfo.baseUrl + this.store.apiInfo.endpoints.movies;
-            axios.get(url, {
-                params:{
-                    api_key: this.store.apiInfo.key,
-                    language: this.store.languageKey,
-                    query:this.store.searchKey,
-                }
-            })
-            .then((response)=> {
-                console.log(response.data.results);
-                this.store.movies=response.data.results;
-            })
-            .catch((error)=> console.log(error));
-        },
-    }
+    // methods:{
+    //     searchMovie(){
+    //         const url =this.store.apiInfo.baseUrl + this.store.apiInfo.endpoints.movies;
+    //         axios.get(url, {
+    //             params:{
+    //                 api_key: this.store.apiInfo.key,
+    //                 language: this.store.languageKey,
+    //                 query:this.store.searchKey,
+    //             }
+    //         })
+    //         .then((response)=> {
+    //             console.log(response.data.results);
+    //             this.store.movies=response.data.results;
+    //         })
+    //         .catch((error)=> console.log(error));
+    //     },
+    // }
 }
 </script>
 
 <template>
-    <h1>Boolflix</h1>
-    <div>
-        <label for="">Ricerca</label>
-        <input type="text" id="search-movie" v-model="store.searchKey">
-        <button @click="searchMovie">Cerca</button>
-    </div>
+    <AppHeader />
 </template>
 
 <style>
